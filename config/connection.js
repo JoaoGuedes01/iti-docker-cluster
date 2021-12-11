3eedfe258c8a95ad13fe40527e47f333192968ed
joaoguedes <joaoguedes.cjp@gmail.com>
2021-12-11 01:49:41 +0000

    connection mais especifica
    connection mais especifica

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,13 @@
 const mongoose = require('mongoose');
 
+let databaseIP = '192.168.0.102';
+let databaseName = 'iti'
 
-const URI = "mongodb://192.168.0.102:27017/iti";
+const URI = "mongodb://" + databaseIP + ":27017/" + databaseName;
 
-
-const connectDB = async() => {
+const connectDB = async () => {
     await mongoose.connect(URI, {
-        useUnifiedTopology: true, 
+        useUnifiedTopology: true,
         useNewUrlParser: true
     });
     console.log('Connected to the Database');
