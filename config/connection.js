ce895c66729bd0e718f79d4f71ec3fcaf2bebed6
joaoguedes <joaoguedes.cjp@gmail.com>
2021-12-13 19:17:50 +0000

    mudei o ip
    mudei o ip

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-let databaseIP = '192.168.0.102';
+let databaseIP = '192.168.0.104';
 let databaseName = 'iti'
 
 const URI = "mongodb://" + databaseIP + ":27017/" + databaseName;
